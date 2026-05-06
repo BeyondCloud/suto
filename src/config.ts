87a424cae3c5c8ba14aa6246432cd4ee3f932bde
fuji <fuji@skymizer.com>
2026-05-06 08:50:38 +0800

    set perfect judge to 220ms

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export interface GameSettings {
 export const DEFAULT_SETTINGS: GameSettings = {
   bpm: 183,
   shrinkLeadMs: 500,
-  perfectJudgeWindowMs: 180,
+  perfectJudgeWindowMs: 220,
   hitboxWidth: 500,
   hitboxHeight: 300,
   checkDepth: 50,
